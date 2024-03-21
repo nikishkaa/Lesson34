88b4352cea27d46fce2295e0fc9be874233c9bcf
nikishkaa <nikita.goutor@gmail.com>
2024-03-22 00:48:13 +0300

    -

--- src/by/itstep/goutor/javalesson/lesson34/model/data/WeekDay.java
+++ src/by/itstep/goutor/javalesson/lesson34/model/data/WeekDay.java
@@ -1,13 +1,13 @@
 package by.itstep.goutor.javalesson.lesson34.model.data;
 
 public class WeekDay {
-    public static final WeekDay MONDAY = new WeekDay("Monday",1);
-    public static final WeekDay TUESDAY = new WeekDay("Tuesday",2);
-    public static final WeekDay WEDNESDAY = new WeekDay("Wednesday",3);
-    public static final WeekDay THURSDAY = new WeekDay("Thursday",4);
-    public static final WeekDay FRIDAY = new WeekDay("Friday",5);
-    public static final WeekDay SATURDAY = new WeekDay("Saturday",6);
-    public static final WeekDay SUNDAY = new WeekDay("Sunday",7);
+    public static final WeekDay MONDAY = new WeekDay("Monday", 1);
+    public static final WeekDay TUESDAY = new WeekDay("Tuesday", 2);
+    public static final WeekDay WEDNESDAY = new WeekDay("Wednesday", 3);
+    public static final WeekDay THURSDAY = new WeekDay("Thursday", 4);
+    public static final WeekDay FRIDAY = new WeekDay("Friday", 5);
+    public static final WeekDay SATURDAY = new WeekDay("Saturday", 6);
+    public static final WeekDay SUNDAY = new WeekDay("Sunday", 7);
 
 
     private final String name;
