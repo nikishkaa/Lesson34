265eaa6381673887932f41859f2b22b2129a8c5f
nikishkaa <nikita.goutor@gmail.com>
2024-03-21 19:26:02 +0300

    Add WeekDay class.Add printer.

--- src/by/itstep/goutor/javalesson/lesson34/controller/Main.java
+++ src/by/itstep/goutor/javalesson/lesson34/controller/Main.java
@@ -1,7 +1,13 @@
 package by.itstep.goutor.javalesson.lesson34.controller;
 
+import by.itstep.goutor.javalesson.lesson34.view.Printer;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+
+
+
+
     }
 }
---
+++ src/by/itstep/goutor/javalesson/lesson34/view/Printer.java
@@ -0,0 +1,7 @@
+package by.itstep.goutor.javalesson.lesson34.view;
+
+public class Printer {
+    public static void print(StringBuilder builder) {
+        System.out.print(builder);
+    }
+}
